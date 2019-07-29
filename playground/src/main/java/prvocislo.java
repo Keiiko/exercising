6758330ebddd1bfeb2baa22de5b2cdc7c17025ee
ja073629 <Jan.Horky@o2its.cz>
2019-07-29 14:56:18 +0200

    uprava pro funkci

--- playground/src/main/java/prvocislo.java
+++ playground/src/main/java/prvocislo.java
@@ -1,24 +1,43 @@
 package net.yetamine.playground;
+import java.util.Scanner;
 
 public class prvocislo {
     public static void main(String[] args) {
-        System.out.println("Je tohle prvocislo?");
-        int i=20;
-        int n=2;
-        //if n je 2 je prvo
-        //if n je pod 1 tak neni
-        //if n%2 neni
-        //skakat po 2 -> 3 5 7
-        boolean b=true;
-        while (n<i) {
-            int mod=i%n;
-            System.out.println(i+" mod "+n+" = "+mod);
-            if (mod == 0) {
-                b=false;
-                break;
-            }
-            n++;
-        }
-        System.out.println(b);
+        System.out.print("Test na prvocisla, zadej cislo: ");
+        Scanner in = new Scanner(System.in);
+        int cislo = in.nextInt();
+        //int cislo = Integer.parseInt(System.console().readLine());
+        System.out.println("Je "+cislo+" prvocislo?");
+        System.out.println(prvocislo(cislo)?"Ano":"Ne");
     }
+
+    public static boolean prvocislo(int i) {
+        if (i < 2) {
+            if (i==2) {
+                //if i je 2 je prvo
+                return true;
+            }
+            //if i je pod 1 tak neni
+            return false;
+        }
+
+        if (i%2 == 0) {
+            return false;
+            //if i%2 neni
+        } else {
+            int n = 3;
+            boolean b = true;
+            while (n < i) {
+                int mod = i % n;
+                System.out.println(i + " mod " + n + " = " + mod);
+                if (mod == 0) {
+                    return false;
+                }
+                //skakat po 2 -> 3 5 7
+                n = n + 2;
+            }
+            return true;
+        }
+    }
+
 }
