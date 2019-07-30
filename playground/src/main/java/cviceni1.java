7b1e936c2347ccae3300c8ee03738efa7ad94e85
ja073629 <Jan.Horky@o2its.cz>
2019-07-30 10:02:01 +0200

    cviceni

---
+++ playground/src/main/java/cviceni1.java
@@ -0,0 +1,47 @@
+package net.yetamine.playground;
+
+//testovani
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class cviceni1 {
+    public static void main(String[] args){
+        System.out.print("Test na prvocisla, zadej koncove cislo: ");
+        Scanner in = new Scanner(System.in);
+        int cislo = in.nextInt();
+        vypisPrvocislaAzPo(cislo);
+    }
+
+    public static void vypisPrvocislaAzPo(int cislo) {
+        // TODO vypis konkretni cisla,
+        int[] t = seznamPrvocisel(cislo);
+        System.out.print(Arrays.toString(t));
+    }
+
+    public static int[] seznamPrvocisel(int maxIn) {
+        int prvocisla=0;
+        boolean[] t = new boolean[maxIn + 1];
+//            t[0]=true;
+//            t[1]=true;
+        for (int i=2; i<=maxIn; i++) {
+            if (!t[i]) {
+                ++prvocisla;
+                for (int j = i*2; j<=maxIn; j += i) {
+                    t[j]=true;
+                }
+            }
+        }
+        int[] arr = new int[prvocisla];
+        int prvocislaIxd = 0;
+        for (int i=2; i<maxIn; i++) {
+            if (!t[i]) {
+                arr[prvocislaIxd] = i;
+                ++prvocislaIxd;
+            }
+        }
+        return arr;
+    }
+
+
+}
